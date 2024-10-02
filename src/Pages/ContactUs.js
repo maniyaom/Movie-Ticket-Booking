0d66c7e79539026ba2fb8a2b8ac0d6698b7e3b98
Ayush <asharma7588@gmail.com>
2024-10-02 16:18:30 +0530

    aboutus contactus and login page responseive for small screens

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -4,7 +4,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import loader_icon from "../assets/icons/loader_icon.gif";
 import "./utils.css";
 import "./AboutUs.css";
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
 
 const ContactUs = () => {
   return (
@@ -21,9 +21,7 @@ const ContactUs = () => {
           >
             Contact US
           </h1>
-          <p
-            style={{ fontWeight: "600", margin: "10px 200px" }}
-          >
+          <p style={{ fontWeight: "600", margin: "10px 200px" }}  className="Contact_Us_Text">
             Thank you for choosing Ticketify! We're here to assist you with any
             questions or concerns you may have. Here's how you can get in touch
             with us:
@@ -75,24 +73,42 @@ const ContactUs = () => {
                 We value your feedback and suggestions. Let us know how we can
                 improve your experience.
               </li>
-              <ul style={{ marginLeft: "2rem", marginTop: '1rem', marginBottom: '2rem' }}>
+              <ul
+                style={{
+                  marginLeft: "2rem",
+                  marginTop: "1rem",
+                  marginBottom: "2rem",
+                }}
+              >
                 <li>
                   <b>Email: </b>feedback@yourmovietickets.com
                 </li>
               </ul>
             </ul>
 
-            <h2 className="title" style={{fontSize: '22px', fontWeight: 500}}>Address</h2>
-            <p style={{ marginLeft: "2rem", fontWeight: 600, opacity: 0.8, marginBottom: '1rem' }}>
+            <h2 className="title" style={{ fontSize: "22px", fontWeight: 500 }}>
+              Address
+            </h2>
+            <p
+              style={{
+                marginLeft: "2rem",
+                fontWeight: 600,
+                opacity: 0.8,
+                marginBottom: "1rem",
+              }}
+            >
               Ticketify private limited, 123 Movie Ave, Surat, Gujarat, 394107,
               India
             </p>
           </div>
-          <p className="card-image-div flex-col" style={{backgroundColor: '#fdcad3', fontWeight: 600}}>
-              Feel free to reach out to us through any of the above channels. We
-              strive to respond to all inquiries within 24 hours during business
-              days. Thank you for choosing Ticketify!
-            </p>
+          <p
+            className="card-image-div flex-col"
+            style={{ backgroundColor: "#fdcad3", fontWeight: 600 }}
+          >
+            Feel free to reach out to us through any of the above channels. We
+            strive to respond to all inquiries within 24 hours during business
+            days. Thank you for choosing Ticketify!
+          </p>
         </div>
       </div>
       <Footer />

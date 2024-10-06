52f90a560d18ccaa9f53cf93fc7968c59caef8d0
Om Maniya <105499099+maniyaom@users.noreply.github.com>
2024-10-06 11:31:38 +0530

    Update AboutUs.js

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import './AboutUs.css'
 import facebook_icon from '../assets/icons/facebook2.png'

81d1872919547d3f65b91e4159d82d073100e54e
Rishi Dholkheria <rishidholkheria2001@gmail.com>
2024-10-07 19:59:26 +0530

    Sorting movies on the basis of Rating, Release Date and Total votes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,38 +1,82 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useFirebase } from "../context/firebase";
-import './Home.css';
-import { Link, useNavigate } from 'react-router-dom';
+import "./Home.css";
+import { Link, useNavigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import Footer from '../components/Footer';
-import Search from '../components/Search';
+import Footer from "../components/Footer";
+import Search from "../components/Search";
 
 const Home = () => {
   const firebase = useFirebase();
   const [allMovies, setAllMovies] = useState([]);
   const [posterPaths, setPosterPaths] = useState({});
+  const [sortType, setSortType] = useState("recent");
 
   const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   const navigate = useNavigate();
   const auth = getAuth();
 
+  const sortMovies = (movies) => {
+    console.log("Sort Clicked", sortType);
+    let sortedMovies = [...movies];
+
+    if (sortType === "avgRating") {
+      const moviesWithAverageRatings = sortedMovies.map((movie) => {
+        const ratingsArray = Object.values(movie.rating);
+        const averageRating =
+          ratingsArray.reduce((acc, curr) => acc + curr, 0) /
+          ratingsArray.length;
+        return { ...movie, averageRating };
+      });
+
+      sortedMovies = moviesWithAverageRatings.sort(
+        (a, b) => b.averageRating - a.averageRating
+      );
+    } else if (sortType === "totalVotes") {
+      const moviesWithHighestVoteCount = sortedMovies.map((movie) => {
+        const ratingsArray = Object.values(movie.rating);
+        const votes = ratingsArray.length;
+        return { ...movie, votes };
+      });
+
+      sortedMovies = moviesWithHighestVoteCount.sort(
+        (a, b) => b.votes - a.votes
+      );
+    } else if (sortType === "recent") {
+      sortedMovies = sortedMovies.sort((a, b) => {
+        const dateA = new Date(a.movieReleaseDate);
+        const dateB = new Date(b.movieReleaseDate);
+        return dateB - dateA;
+      });
+    }
+
+    setAllMovies(sortedMovies);
+  };
+
   onAuthStateChanged(auth, (user) => {
     if (!user) {
-      navigate("/Login")
+      navigate("/Login");
     }
   });
 
   const handleImageLoad = () => {
     setIsImageLoaded(true);
-  }
+  };
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const movies = await firebase.fetchAllMovies();
-        setAllMovies(movies);
+        console.log(movies);
+
+        sortMovies(movies);
+
         // Fetch poster paths for all movies
-        const paths = await Promise.all(movies.map(movie => firebase.fetchMoviePoster(movie.movieId)));
+        const paths = await Promise.all(
+          movies.map((movie) => firebase.fetchMoviePoster(movie.movieId))
+        );
+
         // Create an object with movie IDs as keys and poster paths as values
         const posterPathsObj = {};
         movies.forEach((movie, index) => {
@@ -44,13 +88,25 @@ const Home = () => {
       }
     };
 
-    document.title = 'Home - Book My Show';
+    document.title = "Home - Book My Show";
     fetchMovies();
-  }, []);
+  }, [sortType]);
 
   return (
     <>
-      <Search movies={allMovies} />
+      <div className="search-sort-container">
+        <Search movies={allMovies} />
+        <select
+          onChange={(e) => {
+            setSortType(e.target.value); // This will set the selected value to the state
+          }}
+        >
+          <option value="recent">Latest Release</option>
+          <option value="totalVotes">Total Votes</option>
+          <option value="avgRating">Average Rating</option>
+        </select>
+      </div>
+
       <div className="poster-container">
         {allMovies.map((movie, index) => {
           const { movieReleaseDate, movieTitle, movieGenre, movieId } = movie;
@@ -58,7 +114,10 @@ const Home = () => {
           return (
             <Link to={"/MovieDetails/" + movieId} key={index}>
               <div className="poster">
-                <div className="placeholder shimmer" style={{ width: '222px', height: '340px' }}>
+                <div
+                  className="placeholder shimmer"
+                  style={{ width: "222px", height: "340px" }}
+                >
                   <style>
                     {`
                       .shimmer::before {
@@ -73,20 +132,28 @@ const Home = () => {
                         height: 100%;
                         width: 100%;
                         animation: shimmer 1s infinite;
-                        ${isImageLoaded ? 'z-index: -1;' : ''}
+                        ${isImageLoaded ? "z-index: -1;" : ""}
                        }
                      `}
                   </style>
                   <div className="faux-image-wrapper">
                     <div className="faux-image">
-                      <img src={posterPath}
-                        onLoad={handleImageLoad} />
+                      <img src={posterPath} onLoad={handleImageLoad} />
                     </div>
                   </div>
                 </div>
-                <div className="timestamp myt-10" style={{ fontWeight: 500 }}>{movieReleaseDate}</div>
-                <div className="movie-name myt-5" style={{ fontWeight: 600 }}>{movieTitle}</div>
-                <div className="movie-genre myt-5" style={{ opacity: 0.8, fontSize: 15 }}>{movieGenre}</div>
+                <div className="timestamp myt-10" style={{ fontWeight: 500 }}>
+                  {movieReleaseDate}
+                </div>
+                <div className="movie-name myt-5" style={{ fontWeight: 600 }}>
+                  {movieTitle}
+                </div>
+                <div
+                  className="movie-genre myt-5"
+                  style={{ opacity: 0.8, fontSize: 15 }}
+                >
+                  {movieGenre}
+                </div>
               </div>
             </Link>
           );

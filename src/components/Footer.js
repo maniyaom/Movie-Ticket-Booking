352389a7cd0da80ff64467c638bd39a916538ccb
akankshagoel28 <goelakanksha7379@gmail.com>
2024-10-10 23:40:28 +0530

    made warning changes and the router ones

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
 import facebook_icon from '../assets/icons/facebook2.png'

caf674a9b5100e7030d0fc8b67269f26b9f5256c
SRV30 <142303159+SRV30@users.noreply.github.com>
2024-10-15 21:46:56 +0530

    Changes some design forgot password & signup

--- src/Pages/ForgotPassword.js
+++ src/Pages/ForgotPassword.js
@@ -34,8 +34,41 @@ const ForgotPassword = () => {
   document.title = "Forgot Password";
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-orange-400 to-orange-600">
+    <div className="flex justify-center items-center h-screen bg-white">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
+        <div className="hidden sm:block h-28 w-28 text-indigo-300 absolute a-z-10 -right-20 -bottom-20">
+          <svg
+            id="patternId"
+            width="100%"
+            height="100%"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <defs>
+              <pattern
+                id="b"
+                patternUnits="userSpaceOnUse"
+                width="40"
+                height="40"
+                patternTransform="scale(0.5) rotate(0)"
+              >
+                <rect x="0" y="0" width="100%" height="100%" fill="none" />
+                <path
+                  d="M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5"
+                  strokeWidth="1"
+                  stroke="none"
+                  fill="#f43f5e"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="800%"
+              height="800%"
+              transform="translate(0,0)"
+              fill="url(#b)"
+            />
+          </svg>
+        </div>
+
         <h2 className="text-3xl font-semibold text-center text-gray-800 mb-6">
           Forgot Password
         </h2>
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -225,6 +225,38 @@ const SignUp = () => {
       <div className="flex justify-center align-center h-screen w-screen flex-wrap text-slate-800 ">
         <div className="flex w-full flex-col md:w-1/2 relative sm:w-[30rem] rounded-lg border-gray-400 bg-white shadow-lg px-4">
           <div className="my-auto mx-auto flex flex-col justify-center px-6 pt-8 md:justify-start lg:w-[28rem]">
+            <div className="hidden sm:block h-28 w-28 text-indigo-300 absolute a-z-10 -right-20 -bottom-20">
+              <svg
+                id="patternId"
+                width="100%"
+                height="100%"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <defs>
+                  <pattern
+                    id="b"
+                    patternUnits="userSpaceOnUse"
+                    width="40"
+                    height="40"
+                    patternTransform="scale(0.5) rotate(0)"
+                  >
+                    <rect x="0" y="0" width="100%" height="100%" fill="none" />
+                    <path
+                      d="M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5"
+                      strokeWidth="1"
+                      stroke="none"
+                      fill="#f43f5e"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width="800%"
+                  height="800%"
+                  transform="translate(0,0)"
+                  fill="url(#b)"
+                />
+              </svg>
+            </div>
             <p className="text-center text-3xl font-bold md:text-left md:leading-tight">
               Create your account
             </p>
@@ -494,7 +526,6 @@ const SignUp = () => {
                     className="inline-block text-xs"
                     htmlFor="agree-terms-and-conditions"
                   >
-                    {" "}
                     By clicking the button, you are agreeing to our
                     <a className="underline ml-1" href="#">
                       Terms & Conditions

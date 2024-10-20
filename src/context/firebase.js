ad01df61de3afe22722fc17dd818f96232c7030b
unknown <dindishriya@gmail.com>
2024-10-20 17:32:38 +0530

    Rectified the issue of the previosly pushed PR of addition of feedback section

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -7,12 +7,6 @@ import { Link, useNavigate,} from 'react-router-dom'
 
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
